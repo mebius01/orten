6744e5066b1c08d301a47308e803cf9c421c6148
iv <consmebius@gmail.com>
2018-11-11 21:02:49 +0200

    .

--- orten/settings.py
+++ orten/settings.py
@@ -127,9 +127,11 @@ USE_TZ = True
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_URL = '/static/'
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
