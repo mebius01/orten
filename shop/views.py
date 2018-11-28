5d7edf451e68999ab513f75b4aecde815c3a3c67
iv <consmebius@gmail.com>
2018-11-28 17:43:59 +0200

    not work list product

--- shop/views.py
+++ shop/views.py
@@ -22,14 +22,10 @@ def show_category(request,hierarchy= None):
 		return render(request, 'shop/categories.html', {'instance':instance})
 
 
-def product_list(request, category_slug=None):
-	category = None
+def product_list(request):
 	categories = Category.objects.all()
 	products = Product.objects.filter(available=True)
-	if category_slug:
-		category = get_object_or_404(Category, slug=category_slug)
-		products = products.filter(category=category)
-	return render(request, 'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
+	return render(request, 'shop/product/list.html', {'categories': categories, 'products': products})
 
 
 def product_detail(request, slug):
