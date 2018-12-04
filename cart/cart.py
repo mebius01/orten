b3863f276012013705a2e03ba77c3b3a20419134
iv <consmebius@gmail.com>
2018-12-04 11:43:54 +0200

    products add basket, session is working

--- cart/cart.py
+++ cart/cart.py
@@ -21,7 +21,7 @@ class Cart(object):
 		product_id = str(product.id)
 		if product_id not in self.cart:
 			self.cart[product_id] = {'quantity': 0,
-									 'price': str(product.price)}
+									 'price_uah': str(product.price_uah)}
 		if update_quantity:
 			self.cart[product_id]['quantity'] = quantity
 		else:
@@ -54,8 +54,8 @@ class Cart(object):
 			self.cart[str(product.id)]['product'] = product
 
 		for item in self.cart.values():
-			item['price'] = Decimal(item['price'])
-			item['total_price'] = item['price'] * item['quantity']
+			item['price_uah'] = Decimal(item['price_uah'])
+			item['total_price_uah'] = item['price_uah'] * item['quantity']
 			yield item
 
 
@@ -63,8 +63,8 @@ class Cart(object):
 	def __len__(self):
 		return sum(item['quantity'] for item in self.cart.values())
 
-	def get_total_price(self):
-		return sum(Decimal(item['price']) * item['quantity'] for item in self.cart.values())
+	def get_total_price_uah(self):
+		return sum(Decimal(item['price_uah']) * item['quantity'] for item in self.cart.values())
 
 	def clear(self):
 		del self.session[settings.CART_SESSION_ID]
@@ -78,8 +78,8 @@ class Cart(object):
 
 	def get_discount(self):
 		if self.cupon:
-			return (self.cupon.discount / Decimal('100')) * self.get_total_price()
+			return (self.cupon.discount / Decimal('100')) * self.get_total_price_uah()
 		return Decimal('0')
 
-	def get_total_price_after_discount(self):
-		return self.get_total_price() - self.get_discount() 
+	def get_total_price_uah_after_discount(self):
+		return self.get_total_price_uah() - self.get_discount() 
