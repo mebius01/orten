e61e1fd2446cf573082a3f70d88d461ca4583952
iv <consmebius@gmail.com>
2018-12-14 14:29:44 +0200

    add breadcrumb from django-sitetree work

--- shop/views.py
+++ shop/views.py
@@ -8,31 +8,22 @@ def category(request):
 	list_category = Category.objects.all()
 	return render(request, 'shop/list_category.html', {'list_category': list_category})
 
-def show_category(request,hierarchy=None, tag_id=None):
+def show_category(request,hierarchy=None):
 	category_slug = hierarchy.split('/')
 
 	parent = None
 	root = Category.objects.all()
-	tag = None
-	
-	if tag_id:
-		tag = get_object_or_404(Tag, slug=tag_id)
-		product_list_all = porduct_list_all.filter(tags__in=[tag])
-	print(tag)
+
 	for slug in category_slug[:-1]:
 		parent = root.get(parent=parent, slug = slug)
 	try:
 		instance = Category.objects.get(parent=parent, slug=category_slug[-1])
 	except:
-		cart_product_form = CartAddProductForm()
-		category = Category.objects.filter(slug=category_slug[-2])
 		instance = get_object_or_404(Product, slug = category_slug[-1])
-		return render(request, "shop/postDetail.html", {'instance':instance, 'category':category, 'tag':tag, 'cart_product_form': cart_product_form})
+		return render(request, "shop/postDetail.html", {'instance':instance})
 	else:
 		category = Category.objects.get(slug=category_slug[-1])
 		products = Product.objects.filter(category=category)
-		print(products)
-		print(category)
 		return render(request, 'shop/categories.html', {'instance':instance, 'category':category, 'products': products})
 
 # def product_detail(request, product_slug):
@@ -50,7 +41,7 @@ def product_list(request, tag_id=None):
 	return render(request, 'shop/list.html', {'products': products, 'cart_product_form': cart_product_form})
 
 
-# def product_detail(request, slug):
-# 	print('qq')
-# 	product = get_object_or_404(Product, slug=slug, available=True)
-# 	return render(request, 'shop/product_card.html', {'product': product})
+def product_detail(request, slug):
+	cart_product_form = CartAddProductForm()
+	product = get_object_or_404(Product, slug=slug, available=True)
+	return render(request, 'shop/postDetail.html', {'product': product, 'cart_product_form': cart_product_form})
