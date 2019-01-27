d6d69a48b14ef22bf04a301dddc652e1f31ff4ad
iv <consmebius@gmail.com>
2019-01-27 23:29:01 +0200

    Redis+Celery..

--- orten/settings.py
+++ orten/settings.py
@@ -208,7 +208,9 @@ HAYSTACK_CONNECTIONS = {
 
 # CELERY STUFF
 BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+CELERY_IMPORTS = ('shop.tasks',)
+CELERY_IMPORTS = ('order.tasks',)
