80c546c9a2707bc13bdf25a1539ee49fa57208d4
iv <consmebius@gmail.com>
2019-01-30 00:04:11 +0200

    - render for product_detail

--- shop/views.py
+++ shop/views.py
@@ -30,30 +30,50 @@ def category(request):
 def list_category(request, hierarchy=None):
 	# Раззделяет строку УРЛа на список [категория, подкатегория, подкатегорияПодкатегории, итд]
 	category_slug = hierarchy.split('/')
-
+	print(1, category_slug)
 	parent = None
-	category_all = Category.objects.all()
-
-	# Форма количиства и корзины
-	cart_product_form = CartAddProductForm()
+	category_all = Category.objects.all() # передать в контестный процессор
 
 	for slug in category_slug[:-1]:
+		print(2, slug, parent)
 		parent = category_all.get(parent=parent, slug = slug)
-	try:
-		instance = Category.objects.get(parent=parent, slug=category_slug[-1])
-		print('AAAAAAA', instance)
-	except:
-		instance = get_object_or_404(Product, slug = category_slug[-1])
-		category = Category.objects.get(product=instance)
-		print('BBBBBBB', instance)
-		return render( request, "shop/product_detail.html", {'instance':instance, 'category':category, 'cart_product_form': cart_product_form})
-	else:
-		category = Category.objects.get(slug=category_slug[-1])
-		products = Product.objects.filter(category=category)
-		print('CCCCC', category)
-		return render(request, 'shop/list_category.html', {'instance':instance, 'category':category, 'products': products, 'category_all':category_all, 'cart_product_form': cart_product_form})
+		print(3, parent)
+	instance = Category.objects.get(parent=parent, slug=category_slug[-1])
+	print(4, instance, category_slug[-1])
+	products = Product.objects.filter(category=instance)
+	print(5, products)
+
+	return render(request, 'shop/list_category.html', {'instance':instance, 'category':category, 'products': products, 'category_all':category_all})
 
 
+# def list_category(request, hierarchy=None):
+# 	# Раззделяет строку УРЛа на список [категория, подкатегория, подкатегорияПодкатегории, итд]
+# 	category_slug = hierarchy.split('/')
+
+# 	parent = None
+# 	category_all = Category.objects.all()
+
+# 	# Форма количиства и корзины
+# 	cart_product_form = CartAddProductForm()
+
+# 	for slug in category_slug[:-1]:
+# 		print(slug, parent)
+
+# 		parent = category_all.get(parent=parent, slug = slug)
+# 		print(slug, parent)
+# 	try:
+# 		instance = Category.objects.get(parent=parent, slug=category_slug[-1])
+# 		print('AAAAAAA', instance)
+# 	except:
+# 		instance = get_object_or_404(Product, slug = category_slug[-1])
+# 		category = Category.objects.get(product=instance)
+# 		print('BBBBBBB', instance)
+# 		return render( request, "shop/product_detail.html", {'instance':instance, 'category':category, 'cart_product_form': cart_product_form})
+# 	else:
+# 		category = Category.objects.get(slug=category_slug[-1])
+# 		products = Product.objects.filter(category=category)
+# 		print('CCCCC', category)
+# 		return render(request, 'shop/list_category.html', {'instance':instance, 'category':category, 'products': products, 'category_all':category_all, 'cart_product_form': cart_product_form})
 
 
 def search(request):
