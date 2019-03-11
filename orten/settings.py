cf182d524b1459ba01d7282df0721d7d3ac65dbd
iv <consmebius@gmail.com>
2019-03-11 17:53:58 +0200

    .

--- orten/settings.py
+++ orten/settings.py
@@ -117,16 +117,16 @@ WSGI_APPLICATION = 'orten.wsgi.application'
 
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'orten_db',
-        'USER' : 'iv',
-        'PASSWORD' : '1234',
-        'HOST' : '127.0.0.1',
-        'PORT' : '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'orten_db',
+#         'USER' : 'iv',
+#         'PASSWORD' : '1234',
+#         'HOST' : '127.0.0.1',
+#         'PORT' : '5432',
+#     }
+# }
 
 
 # Password validation
@@ -157,10 +157,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DECIMAL_SEPARATOR = "."
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
@@ -245,12 +246,12 @@ django_heroku.settings(locals())
 
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Static files (CSS, JavaScript, Images)
