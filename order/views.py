3b08636c985f7e17b5bfd9bcb26ceaecebd4d5b5
iv <consmebius@gmail.com>
2019-03-15 16:07:46 +0200

    .

--- order/views.py
+++ order/views.py
@@ -19,7 +19,8 @@ def order_create(request):
 			for item in cart:
 				OrderItem.objects.create(order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
 			cart.clear()
-			return redirect('shop:product_list')
+			order_created.delay(order.id)
+			return render(request, 'order/created.html', {'order': order})
 	else:
 		form = OrderCreateForm()
 	return render(request, 'order/create.html', {'cart': cart, 'form': form})
