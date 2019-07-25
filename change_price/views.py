c20268330a73f7818b6e55ee3dd1e99826a132c6
iv <consmebius@gmail.com>
2019-07-25 22:41:35 +0300

    add app change_price

--- change_price/views.py
+++ change_price/views.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
-
+from shop.models import Category, Services, Product, Polygraphy #, ProductStock
 
 def product_update(request):
+	products = Product.objects.all().order_by('-action', '-image')[:12]
 	# some code
-	ctx = {'data': 'test'}
+	ctx = {'products': products}
 	return render(request, 'product_update.html', ctx)
