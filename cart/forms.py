ebe7c94a55e546ba3ce405d15e4c11c2b01c4087
iv <consmebius@gmail.com>
2020-02-28 00:09:52 +0200

    validator: home, product_detail, category, list_category. replace fiald in shop.forms.py TypedChoiceField -> IntegerField

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,5 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
-	quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+	quantity = forms.IntegerField(initial=1, min_value=1)
 	update = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
